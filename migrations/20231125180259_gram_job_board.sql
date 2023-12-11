1dedfb1ec9ec7c859147df5544e70b1a7ee65b31
Tiras Mwangi <tmwangi599@gmail.com>
2023-12-11 22:00:44 +0300

    migrations schema

---
+++ migrations/20231125180259_gram_job_board.sql
@@ -0,0 +1,9 @@
+-- Add migration script here
+CREATE TABLE IF NOT EXISTS jobs (
+    id serial primary key,
+    user_id varchar not null,
+    company_name varchar not null,
+    location varchar not null,
+    salary_range varchar not null,
+    job_title varchar not null
+);
