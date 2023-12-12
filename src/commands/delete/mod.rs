40a41100a710d79305160af889ea98ac691904a5
Tiras Mwangi <tmwangi599@gmail.com>
2023-12-12 14:46:28 +0300

    added delete_job fn

---
+++ src/commands/delete/mod.rs
@@ -0,0 +1,29 @@
+use teloxide::prelude::*;
+use super::BotResult;
+use crate::SharedState;
+
+pub async fn delete_job(job: String, bot: Bot, msg: Message, state: SharedState) -> BotResult {
+    let query = sqlx::query("delete from jobs where user_id = $1 and job_id = $2")
+    .bind(msg.chat.id.to_string())
+    .bind(job)
+    .execute(&state.pool)
+    .await;
+
+    match query {
+        Ok(_) => {
+            bot.send_message(
+                msg.chat.id,
+                format!("\nJob deleted successfully\nType /myjobs so see all remaining jobs"),
+            )
+            .await?;
+        },
+        Err(_) => {
+            bot.send_message(
+                msg.chat.id,
+                format!("\nYou don't have any jobs with this name."),
+            )
+            .await?;
+        }
+    }
+    Ok(())
+}
