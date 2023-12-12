69b5fbeaf09808bc90efb87e53497d0e890c5108
Tiras Mwangi <tmwangi599@gmail.com>
2023-12-12 09:24:52 +0300

    extracted a myjobs fn refactored functionality to use query_as instead of query

--- src/commands/posts/mod.rs
+++ src/commands/posts/mod.rs
@@ -10,6 +10,8 @@ pub use salary::receive_salary_range;
 pub use teloxide::{dispatching::dialogue::InMemStorage, prelude::*};
 pub use title::receive_job_title;
 
+use crate::SharedState;
+
 pub type PostDialogue = Dialogue<State, InMemStorage<State>>;
 
 #[derive(Clone, Default)]
@@ -29,4 +31,65 @@ pub enum State {
         title: String,
         salary_range: String,
     },
+}
+
+#[derive(sqlx::FromRow)]
+struct Job {
+    id: i32,
+    company_name: String,
+    location: String,
+    salary_range: String,
+    job_title: String,
+}
+
+pub async fn myjobs(bot: Bot, state: SharedState, msg: Message) -> BotResult {
+    let user_exists_query = sqlx::query("SELECT user_id FROM jobs WHERE user_id = $1")
+        .bind(msg.chat.id.0.to_string())
+        .fetch_one(&state.pool)
+        .await;
+
+    match user_exists_query {
+        Ok(_) => {
+            let query = sqlx::query_as::<_, Job>("SELECT * FROM jobs WHERE user_id = $1")
+                .bind(msg.chat.id.0.to_string())
+                .fetch_all(&state.pool)
+                .await;
+
+            match query {
+                Ok(jobs) => {
+                    let mut job_string = String::new();
+
+                    for job in jobs {
+                        let formatted_string = format!(
+                            "Company name: {}
+                        \nTitle: {}
+                        \nSalary Range: {}
+                        \nLocation: {}\n\n",
+                            job.company_name, job.job_title, job.salary_range, job.location
+                        );
+
+                        job_string.push_str(&formatted_string);
+                    }
+                    bot.send_message(msg.chat.id, format!("{job_string}\n"))
+                        .await?;
+                }
+                Err(_) => {
+                    bot.send_message(
+                        msg.chat.id,
+                        format!("\nYou haven't posted any jobs. To start type /post"),
+                    )
+                    .await?;
+                }
+            }
+        }
+        Err(_) => {
+            bot.send_message(
+                msg.chat.id,
+                format!("\nYou haven't posted any jobs. To start type /post"),
+            )
+            .await?;
+        }
+    }
+
+    Ok(())
 }
