1774247f761f3a78c79d29fd1b0a64e17abd681c
Tiras Mwangi <tmwangi599@gmail.com>
2023-12-12 14:46:47 +0300

    added delete and edit commands

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,10 +1,15 @@
 pub mod posts;
+pub mod edits;
+pub mod delete;
 
 use crate::SharedState;
 use posts::*;
-use sqlx::Row;
 use std::error::Error;
-use teloxide::{dispatching::dialogue::InMemStorage, utils::command::BotCommands};
+use teloxide::{
+    dispatching::dialogue::InMemStorage, 
+    utils::command::BotCommands,
+    prelude::*
+};
 
 pub type BotResult = Result<(), Box<dyn Error + Send + Sync>>;
 
@@ -20,6 +25,10 @@ enum Command {
     Post,
     #[command(description = "see all the jobs you posted")]
     MyJobs,
+    #[command(description = "handles editing jobs")]
+    Edit,
+    #[command(description = "handles deleting jobs. Use the code name to delete a job")]
+    Delete(String),
 }
 
 async fn answer(
@@ -33,58 +42,18 @@ async fn answer(
         Command::Help => {
             bot.send_message(msg.chat.id, Command::descriptions().to_string())
                 .await?;
-        }
+        },
         Command::Post => {
             bot.send_message(msg.chat.id, "Let's start! What's your company name?")
                 .await?;
             dialogue.update(State::ReceiveCompanyName).await?;
-        }
-        Command::MyJobs => {
-            let user_exists_query = sqlx::query("SELECT user_id FROM jobs WHERE user_id = $1")
-                .bind(msg.chat.id.0.to_string())
-                .fetch_one(&state.pool)
-                .await;
-
-            match user_exists_query {
-                Ok(_) => {
-                    let query = sqlx::query("SELECT * FROM jobs WHERE user_id = $1")
-                        .bind(msg.chat.id.0.to_string())
-                        .fetch_all(&state.pool)
-                        .await;
-
-                    match query {
-                        Ok(pg_rows) => {
-                            let mut job_string = String::new();
-
-                            for job in pg_rows {
-                                let company_name: String = job.get("company_name");
-                                let salary_range: String = job.get("salary_range");
-                                let location: String = job.get("location");
-                                let title: String = job.get("job_title");
-                                let formatted_string = format!("Company name: {company_name}\nTitle: {title}\nSalary Range: {salary_range}\nLocation: {location}\n\n");
-                                job_string.push_str(&formatted_string);
-                            }
-                            bot.send_message(msg.chat.id, format!("{job_string}\n"))
-                                .await?;
-                        }
-                        Err(_) => {
-                            bot.send_message(
-                                msg.chat.id,
-                                format!("\nYou haven't posted any jobs. To start type /post"),
-                            )
-                            .await?;
-                        }
-                    }
-                }
-                Err(_) => {
-                    bot.send_message(
-                        msg.chat.id,
-                        format!("\nYou haven't posted any jobs. To start type /post"),
-                    )
-                    .await?;
-                }
-            }
-        }
+        },
+        Command::MyJobs => posts::myjobs(bot, state, msg).await?,
+        Command::Edit => {
+            bot.send_message(msg.chat.id, "Let's edit this")
+                .await?;
+        },
+        Command::Delete(job) => delete::delete_job(job, bot, msg, state).await?
     }
     Ok(())
 }
@@ -107,30 +76,7 @@ pub async fn run(state: SharedState) {
                     .filter_command::<Command>()
                     .endpoint(answer),
             )
-            .branch(
-                Update::filter_message()
-                    .enter_dialogue::<Message, InMemStorage<State>, State>()
-                    .branch(dptree::case![State::ReceiveCompanyName].endpoint(receive_company_name))
-                    .branch(
-                        dptree::case![State::ReceiveTitle { company_name }]
-                            .endpoint(receive_job_title),
-                    )
-                    .branch(
-                        dptree::case![State::ReceiveSalaryRange {
-                            company_name,
-                            title
-                        }]
-                        .endpoint(receive_salary_range),
-                    )
-                    .branch(
-                        dptree::case![State::ReceiveLocation {
-                            company_name,
-                            title,
-                            salary_range
-                        }]
-                        .endpoint(receive_location),
-                    ),
-            )
+            .branch(posts::post_updates())
             .branch(Update::filter_message().endpoint(handle_other_updates)),
     )
     .dependencies(dptree::deps![state, InMemStorage::<State>::new()])
